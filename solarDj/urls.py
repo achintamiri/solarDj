1f02cda3c93b1af2c0fe113d6a82200658b98702
achintamiri <achintamiri@users.noreply.github.com>
2019-10-16 20:43:06 +0100

    adding json data

--- solarDj/urls.py
+++ solarDj/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from solarDj import views
-
+from django.urls import path
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'data', views.data)
+    #url(r'^admin/', admin.site.urls),
+    #url(r'data', views.data)
+    path('dailymonth', views.dailymonth, name='dailymonth'),
+    path('yearly', views.yearly, name='yearly'),
 ]
--- solarDj/views.py
+++ solarDj/views.py
@@ -12,12 +12,20 @@ from django.http import JsonResponse
 from django.core import serializers
 from django.conf import settings
 import json
+import os
 #from solarDj.cylrealtimepy.ucd_demo import make_datalog_query
 
 # Create your views here.
 @api_view(["GET"])
-def data(APIView):
-        data = json.loads(DailyMonthResult.json)
-        return JsonResponse(data)
+def yearly(request):
+        setroot = os.path.dirname(os.path.dirname(__file__))
+        with open(os.path.join(setroot, r'solarDj/YearlyResult.json'), 'r') as myfile:
+                data = myfile.read()
+        return JsonResponse(data,safe=False)
+def dailymonth(request):
+        setroot = os.path.dirname(os.path.dirname(__file__))
+        with open(os.path.join(setroot, r'solarDj/DailyMonthResult.json'), 'r') as myfile:
+                data = myfile.read()
+        return JsonResponse(data,safe=False)
     #except ValueError as e:
      #   return Response(e.args[0],status.HTTP_400_BAD_REQUEST)
